52804c1c57ccfd03b02fd8b2c3ec02ae63bd85f2
TheRealMilesLee <grandtheftauto710@outlook.com>
2024-04-10 23:14:53 -0700

    Delete the tmp files

---
+++ ComplierTestFolder/FindSubsets.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Function to generate all subsets of a given set
+void generateSubsets(vector<int> &S, vector<int> &subset, int index);
+
+// Main function
+int main()
+{
+  // Example set S
+  vector<int> S = {1, 2, 3, 4};
+
+  // Empty subset to start with
+  vector<int> subset;
+
+  // Generate subsets
+  generateSubsets(S, subset, 0);
+
+  return 0;
+}
+
+void generateSubsets(vector<int> &S, vector<int> &subset, int index)
+{
+  for (size_t loop = 0; loop < S.size(); loop++)
+  {
+    cout << S[loop] << " ";
+  }
+}
--- ComplierTestFolder/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./a.exe IJ >IJ.txt
-./a.exe IK >IK.txt
-./a.exe KJ >KJ.txt
