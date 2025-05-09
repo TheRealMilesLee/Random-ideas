c385e35cc76991f4821934c13b5152a1e98be979
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-05-08 18:30:56 -0700

    Update Benchmark

--- ComplierTestFolder/MatrixMul.cpp
+++ ComplierTestFolder/MatrixMul.cpp
@@ -1,6 +1,68 @@
 #include <iostream>
+#include <vector>
+#include <thread>
+#include <algorithm>
 
 using namespace std;
+
+void matrix_mul(vector<vector<int>> &src1,
+                vector<vector<int>> &src2,
+                vector<vector<int>> &dst,
+                size_t blockSize,
+                size_t start,
+                size_t end);
+
+void parallel_computing_simple_multithread(
+    std::vector<std::vector<int>> &matrix1,
+    std::vector<std::vector<int>> &matrix2,
+    std::vector<std::vector<int>> &result,
+    size_t block_size);
+int main()
+{
+  // Default values
+  size_t matrix_size = 4096;
+  size_t block_size = 128;
+
+  // Initialize matrices
+  vector<vector<int>> src1(matrix_size, vector<int>(matrix_size));
+  vector<vector<int>> src2(matrix_size, vector<int>(matrix_size));
+  vector<vector<int>> dst(matrix_size, vector<int>(matrix_size, 0));
+
+  for (size_t row = 0; row < matrix_size; row++)
+  {
+    for (size_t col = 0; col < matrix_size; col++)
+    {
+      src1[row][col] = static_cast<int>(row) + static_cast<int>(col);
+      src2[row][col] = static_cast<int>(row) + static_cast<int>(col);
+    }
+  }
+
+  auto start_time = std::chrono::high_resolution_clock::now();
+  matrix_mul(src1, src2, dst, block_size, 0, src1.size());
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+      end_time - start_time);
+
+  double seconds = static_cast<double>(duration.count()) / 1000000.0;
+
+  cout << "Matrix multiplication time with single thread: " << seconds
+       << " seconds or " << duration.count() % 1000000 << " microseconds"
+       << endl;
+
+  auto start_time_para = std::chrono::high_resolution_clock::now();
+  parallel_computing_simple_multithread(src1, src2, dst, block_size);
+  auto end_time_para = std::chrono::high_resolution_clock::now();
+  auto duration_para = std::chrono::duration_cast<std::chrono::microseconds>(
+      end_time_para - start_time_para);
+
+  double seconds_para =
+      static_cast<double>(duration_para.count()) / 1000000.0;
+
+  cout << "Matrix multiplication time with single thread: " << seconds_para
+       << " seconds or " << duration_para.count() % 1000000 << " microseconds"
+       << endl;
+  return 0;
+}
 
 void matrix_mul(vector<vector<int>> &src1,
                 vector<vector<int>> &src2,
@@ -34,36 +96,28 @@ void matrix_mul(vector<vector<int>> &src1,
   }
 }
 
-int main()
+void parallel_computing_simple_multithread(
+    std::vector<std::vector<int>> &matrix1,
+    std::vector<std::vector<int>> &matrix2,
+    std::vector<std::vector<int>> &result,
+    size_t block_size)
 {
-  // Default values
-  size_t matrix_size = 4096;
-  size_t block_size = 128;
+  std::vector<std::thread> threads;
 
-  // Initialize matrices
-  vector<vector<int>> src1(matrix_size, vector<int>(matrix_size));
-  vector<vector<int>> src2(matrix_size, vector<int>(matrix_size));
-  vector<vector<int>> dst(matrix_size, vector<int>(matrix_size, 0));
-
-  for (size_t row = 0; row < matrix_size; row++)
+  for (size_t i = 0; i < matrix1.size(); i += block_size)
   {
-    for (size_t col = 0; col < matrix_size; col++)
-    {
-      src1[row][col] = static_cast<int>(row) + static_cast<int>(col);
-      src2[row][col] = static_cast<int>(row) + static_cast<int>(col);
-    }
+    threads.push_back(std::thread(
+        [&matrix1, &matrix2, &result, block_size, i]()
+        {
+          size_t end = std::min(i + block_size, matrix1.size());
+          matrix_mul(matrix1, matrix2, result, block_size, i, end);
+        }));
   }
 
-  auto start_time = std::chrono::high_resolution_clock::now();
-  matrix_mul(src1, src2, dst, block_size, 0, src1.size());
-  auto end_time = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
-      end_time - start_time);
+  for (auto &t : threads)
+  {
+    t.join();
+  }
 
-  double seconds = static_cast<double>(duration.count()) / 1000000.0;
-
-  cout << "Matrix multiplication time with single thread: " << seconds
-       << " seconds or " << duration.count() % 1000000 << " microseconds"
-       << endl;
-  return 0;
+  threads.clear();
 }
